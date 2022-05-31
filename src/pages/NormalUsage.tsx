b3ddb4fd42982e68fed91dfda685f4c5dcfa648f
andreseichi <andre_seichi@hotmail.com>
2022-05-30 23:07:15 -0300

    consuming api

--- src/pages/NormalUsage.tsx
+++ src/pages/NormalUsage.tsx
@@ -1,10 +1,26 @@
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
+import axios from 'axios';
+
+interface Repository {
+  id: number;
+  name: string;
+  url: string;
+}
 
 export function NormalUsage() {
   const valueRef = useRef(1);
+  const [repos, setRepos] = useState<Repository[]>([]);
 
   useEffect(() => {
     console.log(`mounted ${valueRef.current}`);
+
+    // most common usage of consuming API
+    axios
+      .get('https://api.github.com/users/andreseichi/repos')
+      .then(({ data }) => {
+        console.log(data);
+        setRepos(data);
+      });
 
     valueRef.current++;
   }, []);
@@ -12,6 +28,13 @@ export function NormalUsage() {
   return (
     <>
       <h1>Check console log</h1>
+
+      {repos?.map((repo) => (
+        <div key={repo.id}>
+          <span>{repo.name}</span>
+          <span>{repo.url}</span>
+        </div>
+      ))}
     </>
   );
 }
